365a05647920a513237eb439f313b8d6c24b9f60
Hisen Zhang <zhangz29@rpi.edu>
2022-10-31 15:46:47 -0400

    name build subdir by time

--- CVManager.py
+++ CVManager.py
@@ -24,28 +24,27 @@ def img2b64(imgpath):
         return base64.b64encode(f.read()).decode()
 
 
-def render_html(db):
+def render_html(db, outpath):
     global html_string
     template_loader = jinja2.FileSystemLoader(searchpath="./theme")
     template_env = jinja2.Environment(loader=template_loader)
     template_file = "layout.html"
     template = template_env.get_template(template_file)
     html_string = template.render(
-        utcnow= datetime.datetime.now(datetime.timezone.utc).isoformat(),
+        utcnow=datetime.datetime.now(datetime.timezone.utc).isoformat(),
         profile=profile,
         person=db['contact'],
         exp=db['experience'],
         img_string=img2b64(os.path.join(
             profile['input.path'], profile['input.logo'])))
 
-    html_path = os.path.join(
-        profile['output.path'], profile['output.filename']) + '.html'
+    html_path = outpath + '.html'
     html_file = open(html_path, 'w')
     html_file.write(html_string)
     html_file.close()
 
 
-def render_pdf(db):
+def render_pdf(db, outpath):
 
     path_to_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
     config = pdfkit.configuration(wkhtmltopdf=path_to_wkhtmltopdf)
@@ -60,18 +59,28 @@ def render_pdf(db):
     }
 
     if html_string is None:
-        render_html(db)
+        render_html(db, outpath)
 
-    pdfkit.from_string(html_string, output_path=os.path.join(
-        profile['output.path'], profile['output.filename']) + '.pdf',
-        configuration=config, options=options)
+    pdfkit.from_string(html_string, output_path=outpath + '.pdf',
+                       configuration=config, options=options)
 
 
 outputs = profile['output.type']
+
+if outputs is not None:
+    ts = datetime.datetime.today().isoformat().split('T')[0]
+    time_dir = ts + '-' + sys.argv[1].split('/')[-1].split('.')[0]
+    try:
+        os.mkdir(os.path.join(profile['output.path'], time_dir))
+    except FileExistsError:
+        pass
+    outpath = os.path.join(
+        profile['output.path'], time_dir, profile['output.filename']+ ' ' +ts)
+
 if 'HTML' in outputs:
     import jinja2
-    render_html(db)
+    render_html(db, outpath)
 if 'PDF' in outputs:
     import jinja2
     import pdfkit
-    render_pdf(db)
+    render_pdf(db, outpath)
