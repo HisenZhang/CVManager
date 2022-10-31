b3c4dc558316ff70504f8a2e1eaea476b2a12ffd
Hisen Zhang <zhangz29@rpi.edu>
2022-10-31 13:24:50 -0400

    add local files

---
+++ CVManager.py
@@ -0,0 +1,75 @@
+import os
+import sys
+import pprint
+import base64
+from yamldb import YamlDB
+
+if len(sys.argv) != 2:
+    print('Usage: CVManager.py PATH_TO_PROFILE')
+    sys.exit()
+
+
+profile = YamlDB(filename=sys.argv[1])
+db = YamlDB(filename=os.path.join(
+            profile['input.path'], profile['input.data']))
+
+html_string = str()
+
+# pprint.pprint(db.experience)
+
+
+def img2b64(imgpath):
+    with open(imgpath, 'rb') as f:
+        return base64.b64encode(f.read()).decode()
+
+
+def render_html(db):
+    global html_string
+    template_loader = jinja2.FileSystemLoader(searchpath="./theme")
+    template_env = jinja2.Environment(loader=template_loader)
+    template_file = "layout.html"
+    template = template_env.get_template(template_file)
+    html_string = template.render(
+        profile=profile,
+        person=db['contact'],
+        exp=db['experience'],
+        img_string=img2b64(os.path.join(
+            profile['input.path'], profile['input.logo'])))
+
+    html_path = os.path.join(
+        profile['output.path'], profile['output.filename']) + '.html'
+    html_file = open(html_path, 'w')
+    html_file.write(html_string)
+    html_file.close()
+
+
+def render_pdf(db):
+
+    path_to_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+    config = pdfkit.configuration(wkhtmltopdf=path_to_wkhtmltopdf)
+    options = {
+        'page-size': 'Letter',
+        'margin-top': '0.5in',
+        'margin-right': '0.8in',
+        'margin-bottom': '0.5in',
+        'margin-left': '0.8in',
+        'encoding': "UTF-8",
+        "dpi": "300",
+    }
+
+    if html_string is None:
+        render_html(db)
+
+    pdfkit.from_string(html_string, output_path=os.path.join(
+        profile['output.path'], profile['output.filename']) + '.pdf',
+        configuration=config, options=options)
+
+
+outputs = profile['output.type']
+if 'HTML' in outputs:
+    import jinja2
+    render_html(db)
+if 'PDF' in outputs:
+    import jinja2
+    import pdfkit
+    render_pdf(db)
